bfa7adaaf4836bd1b91247d27f6c95f285555a48
liangdong <1789719643@qq.com>
2022-02-15 15:03:44 +0800

    fix:update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/workshop',
+  base:'/docs',
   server: {
     host: '0.0.0.0',
     port: 9527,
